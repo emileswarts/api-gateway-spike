31de300e8f6ef72f1f5d660f3afb628c853ee37b
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-01-23 15:36:22 +0000

    add api key

--- apigw.tf
+++ apigw.tf
@@ -29,6 +29,29 @@ resource "aws_api_gateway_integration" "tracks_post_integration" {
   uri                     = "https://en.wikipedia.org/wiki/Lolcat"
 }
 
+resource "aws_api_gateway_resource" "tracks_2" {
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  parent_id   = aws_api_gateway_rest_api.apigw.root_resource_id
+  path_part   = "/"
+}
+
+resource "aws_api_gateway_method" "tracks_2_post" {
+  rest_api_id      = aws_api_gateway_rest_api.apigw.id
+  resource_id      = aws_api_gateway_resource.tracks_2.id
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_integration" "tracks_2_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.apigw.id
+  resource_id             = aws_api_gateway_resource.tracks_2.id
+  http_method             = aws_api_gateway_method.tracks_2_post.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = "https://en.wikipedia.org/wiki/Lolcat"
+}
+
 resource "aws_api_gateway_deployment" "live" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
   stage_name  = "live"
@@ -38,10 +61,32 @@ resource "aws_api_gateway_deployment" "live" {
 
   depends_on = [
     aws_api_gateway_method.tracks_post,
-    aws_api_gateway_integration.tracks_post_integration
+    aws_api_gateway_integration.tracks_post_integration,
+    aws_api_gateway_method.tracks_2_post,
+    aws_api_gateway_integration.tracks_2_post_integration
   ]
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_api_gateway_api_key" "api_keys" {
+  name  = "fake-mapps"
+}
+
+resource "aws_api_gateway_usage_plan" "default" {
+  name = "hmpps-integration-api"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.apigw.id
+    stage  = aws_api_gateway_deployment.live.stage_name
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "main" {
+
+  key_id        = aws_api_gateway_api_key.api_keys.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.default.id
 }
